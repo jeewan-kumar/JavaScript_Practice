cc2029389d417eb19b31e73727ff9f41ae699a23
Jeewan_Kumar <jeevank028@gmail.com>
2024-04-05 15:18:40 +0530

    2nd

---
+++ Practice Question (Part 3)/index.js
@@ -0,0 +1,34 @@
+
+// let arr = [1, 2, 3, 8, 10];
+// let n=2;
+// console.log(arr.slice(0, n));
+// console.log(arr.slice(arr.length-n));
+
+//let str = "hello";
+// let result = str.trim();
+// if (str == result) {
+//     console.log("Not blank");
+// } else {
+//     console.log("blank")
+// }
+
+// let str = prompt("please enter a string: ");
+
+// if (str.length == 0) {
+//     console.log("string is empty");
+// } else {
+//     console.log("string is not empty");
+// }
+
+// let str = "ApNaCoLlEgE";
+// let idx = 4;
+
+// if (str[idx] == str[idx].toLowerCase()) {
+//     console.log("character is lowercase");
+// } else {
+//     console.log("character is not lowercase");
+// }
+
+let str = prompt("please enter a string");
+console.log(`original string = ${str}`);
+console.log(`String wihout space = ${str.trim()}`);
