e6972a55a9a812e57be9b5e1ccd16730e6635209
Jeewan_Kumar <jeevank028@gmail.com>
2024-04-13 21:21:34 +0530

    4th

---
+++ Program/Q2.js
@@ -0,0 +1,13 @@
+/* js program to add two number */
+
+const num1 = 10;
+const num2 = 20;
+const sum = num1 +num2;
+console.log(`Sum of two number : ${sum}`);
+
+const nu1 = parseInt(prompt("Enter first Number"));
+const nu2 = parseInt(prompt("Enter Second Number"));
+const sum1 = nu1 + nu2;
+console.log(`Sum of two number : ${sum1}`);
+document.write(`sum of two number : ${sum1}`);
+
